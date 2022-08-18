430c29bb783d6d75410078afac90b42fb04d85e8
gTarcizo <90656673+gTarcizo@users.noreply.github.com>
2022-08-18 20:58:06 -0300

    Funcoes de adicionar e remover tasks concluidas

--- listatarefas/.eslintrc.js
+++ listatarefas/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
       Atomics: 'readonly',
       SharedArrayBuffer: 'readonly',
     },
-    parser: '@babel/eslint-parser',
+    parser: '@eslint',
     parserOptions: {
       ecmaFeatures: {
         jsx: true,
--- listatarefas/src/components/Main.js
+++ listatarefas/src/components/Main.js
@@ -1,14 +1,65 @@
 import React , { Component } from "react";
-
 //Form
 import { FaPlus } from 'react-icons/fa'
+
+//tarefas
+import { FaEdit, FaWindowClose } from "react-icons/fa";
 
 import './Main.css'
 
 export default class Main extends Component {
     state = {
         novaTarefa: '',
+        tarefas: [],
+        index: -1,
     };
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        const { tarefas, index } = this.state
+        let { novaTarefa } = this.state
+        novaTarefa = novaTarefa.trim()
+
+        if(novaTarefa === '') return
+        if(tarefas.indexOf(novaTarefa) !== -1) return
+
+        const novasTarefas = [...tarefas]
+
+        if(index === -1) {
+            this.setState({
+                tarefas: [...novasTarefas,  novaTarefa],
+                novaTarefa : '',
+            })
+        }else{
+            novasTarefas[index] = novaTarefa;
+            
+            this.setState({
+                tarefas: [...novasTarefas],
+                index: -1,
+            })
+        }
+    }
+
+    editTask = (e, index) => {
+        const { tarefas } = this.state
+
+        this.setState({
+            index,
+            novaTarefa: tarefas[index],
+        })
+        return
+    }
+
+    deleteTask = (e, index) => {
+        const { tarefas } = this.state
+        const novasTarefas = [...tarefas]
+        novasTarefas.splice(index, 1)
+        this.setState({
+            tarefas: [...novasTarefas]
+        })
+        return
+    }
 
     inputChange = (event) => {
         this.setState({
@@ -17,16 +68,28 @@ export default class Main extends Component {
     }
 
     render() {
-        const { novaTarefa } = this.state;
+        const { novaTarefa, tarefas } = this.state;
 
         return (
             <div className="Main">
                 <h1>Lista de Tarefas </h1>
 
-                <form action="#" className="form">
+                <form onSubmit={this.handleSubmit} action="#" className="form">
                     <input onChange={this.inputChange} type='text' value={novaTarefa}/>
                     <button type='submit'><FaPlus/></button>
                 </form>
+
+                <ul className="tarefas">
+                    {tarefas.map((tarefa, index) => (
+                        <li key={tarefa}>
+                            {tarefa + ' '}
+                            <span>
+                                <FaEdit onClick={(e) => this.editTask(e, index)} className="buttonEdit"/>
+                                <FaWindowClose onClick={(e) => this.deleteTask(e, index)} className="buttonEdit"/>
+                            </span>
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
